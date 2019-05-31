0903e2271672856511606000247093e1c7a0f9c6
yume <510372582@qq.com>
2019-05-31 17:40:33 +0800

    texi

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var fee;
+    if (inputs.distance<=2){
+fee=6;
+    }else if(inputs.distance<=8){
+fee=0.8*inputs.distance;
+    }else{
+fee=6.4+(inputs.distance-8)*1.6;
+    }
+    if(inputs.parkTime!=0){
+        fee+=inputs.parkTime*0.25;
+    }
+    fee=Math.round(fee);
+    return fee;
 };
